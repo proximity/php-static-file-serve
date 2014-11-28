fa668aafcc36781ec2df6b3dfa2d4915fda27783
Dylan Jennings <dylan.jennings@proximity.co.nz>
2014-11-28 13:39:24 +1300

    Add support for default files to be served if a directory is requested.

--- php/config.sample.php
+++ php/config.sample.php
@@ -2,4 +2,7 @@
 $config = array(
 	'folder_location'	=> '/my/folder',
 	'magic_file'		=> null,
+	'default_files'		=> array(
+		'index.html'
+	),
 );
--- php/index.php
+++ php/index.php
@@ -29,8 +29,43 @@ if (substr($file, 0, 1) !== '/')
 }
 
 // Concatenate folder and file, and check it exists
-$file = $folder . $file;
-if (!file_exists($file))
+$fullPath = $folder . $file;
+$searchFiles = array();
+if (is_dir($fullPath))
+{
+	// Requested path is a folder. Remove trailing slashes.
+	while (substr($fullPath, 0, -1) === '/')
+	{
+		$fullPath = substr($fullPath, 0, strlen($fullPath) - 1);
+	}
+
+	// Add all "default" files to the search array.
+	foreach ($config['default_files'] as $default)
+	{
+		$searchFiles[] = $fullPath . '/' . $default;
+	}
+}
+else
+{
+	// Requested path points to a file, so look directly for that file.
+	$searchFiles[] = $fullPath;
+}
+
+// Set $file to null until we've found a matching file.
+$file = null;
+
+// Loop through $searchFiles until we find a file that exists.
+foreach($searchFiles as $searchFile)
+{
+	if (file_exists($searchFile) && !is_dir($searchFile))
+	{
+		$file = $searchFile;
+		break;
+	}
+}
+
+// If file is still null, it didn't find any files. Send 404 error.
+if (!$file)
 {
 	header("HTTP/1.0 404 Not Found");
 	die("404 Not Found");
